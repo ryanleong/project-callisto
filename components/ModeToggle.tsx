f200f7fd1fd2742fbc9bc7d7dcde37a674c47ef4
Ryan Leong <hello@ryanleong.net>
2024-06-26 21:51:06 +0800

    Set up shadcn/ui with dark mode toggle

---
+++ components/ModeToggle.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import * as React from "react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+
+export function ModeToggle() {
+  const { setTheme } = useTheme()
+
+  return (
+    <>
+      <Button onClick={() => setTheme('light')}>Light</Button>
+      <Button variant="outline" onClick={() => setTheme('dark')}>Dark</Button>
+      <Button onClick={() => setTheme('system')}>System</Button>
+    </>
+  )
+}
